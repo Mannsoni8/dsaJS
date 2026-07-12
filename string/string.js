1760f513aa173723eda523fef1cf277d66ff8487
Mannsoni8 <mannsoni4352@gmail.com>
2026-07-12 20:31:29 +0530

    add some string problem

--- string/string.js
+++ string/string.js
@@ -79,3 +79,100 @@
 //   ans = ans + (first + middel + last) + " ";
 // }
 // console.log(ans);
+
+//frequency of each character
+
+// let s = "madnASDcaAASAD";
+// let arr = new Array(128).fill(0);
+// let n = s.length;
+
+// for (let i = 0; i < n; i++) {
+//   let ascii = s.charCodeAt(i);
+
+//   arr[ascii] = arr[ascii] + 1;
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     console.log(String.fromCharCode(i) + "->" + arr[i]);
+//   }
+// }
+
+//one more method
+
+// const str = "programming";
+// const freq = {};
+
+// for (let char of str) {
+//   if (freq[char]) {
+//     freq[char]++;
+//   } else {
+//     freq[char] = 1;
+//   }
+// }
+
+// console.log(freq);
+
+//freq of small char only
+
+// let s = "madnadnkdadsvs";
+// let arr = new Array(26).fill(0);
+// let n = s.length;
+
+// for (let i = 0; i < n; i++) {
+//   let ascii = s.charCodeAt(i);
+
+//   arr[ascii - 97] = arr[ascii - 97] + 1; // substract 97 because to fill the index as 97 is the ascii value of 'a' which is starting char
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     console.log(String.fromCharCode(i + 97) + "->" + arr[i]);
+//   }
+// }
+
+// Anagram String
+
+// let s1 = "car";
+// let s2 = "arc";
+
+// if (s1.length !== s2.length) {
+//   console.log("Not anagram");
+// } else {
+//   let arr = new Array(26).fill(0);
+//   for (let i = 0; i < s1.length; i++) {
+//     let ascii = s1.charCodeAt(i);
+//     arr[ascii - 97] = arr[ascii - 97] + 1;
+//   }
+//   for (let i = 0; i < s1.length; i++) {
+//     let ascii = s2.charCodeAt(i);
+//     arr[ascii - 97] = arr[ascii - 97] - 1;
+//   }
+
+//   let isAnagram = true;
+
+//   for (let i = 0; i < 26; i++) {
+//     if (arr[i] !== 0) {
+//       isAnagram = true;
+//       break;
+//     }
+//   }
+//   console.log(isAnagram?"Anagram":"Not anagram")
+// }
+
+//MAximum number of word in sentence
+
+// let sentences = [
+//   "alice and bob love leetcode",
+//   "i think so too",
+//   "this is great thanks very much",
+// ];
+
+// let maxWord = 0;
+
+// for (let s of sentences) {
+//   let word = s.split("");
+//   maxWord = Math.max(maxWord, word.length);
+// }
+
+// console.log(maxWord);
