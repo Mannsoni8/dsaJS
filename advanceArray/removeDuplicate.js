911d1f86c99e80620c4c3dc1c8cf39685e4288d3
Mannsoni8 <mannsoni4352@gmail.com>
2026-08-07 14:18:35 +0530

    add implementations for merging two sorted arrays and removing duplicates

---
+++ advanceArray/removeDuplicate.js
@@ -0,0 +1,11 @@
+let nums = [1, 1, 2];
+
+if (nums.length === 0) return 0;
+let k = 1;
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] !== nums[k - 1]) {
+    nums[k] = nums[i];
+    k++;
+  }
+}
+console.log(k);
